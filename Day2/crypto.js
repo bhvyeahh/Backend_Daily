9164305d2f779eeb23464de52d973bea2917c043
Bhavya Rathore <bhavyarathore575@gmail.com>
2025-04-10 15:38:14 +0530

    Add files via upload

---
+++ Day2/crypto.js
@@ -0,0 +1,21 @@
+process.env.UV_THREADPOOL_SIZE = 5
+const crypto = require("crypto")
+
+
+
+let start = Date.now();
+crypto.pbkdf2("password-1", "salt1", 100000, 1024, "sha512", ()=>{
+console.log(`${Date.now() - start}ms   Done`)
+})
+crypto.pbkdf2("password-1", "salt1", 100000, 1024, "sha512", ()=>{
+    console.log(`${Date.now() - start}ms   Done`)
+    })
+crypto.pbkdf2("password-1", "salt1", 100000, 1024, "sha512", ()=>{
+    console.log(`${Date.now() - start}ms   Done`)
+    })
+crypto.pbkdf2("password-1", "salt1", 100000, 1024, "sha512", ()=>{
+    console.log(`${Date.now() - start}ms   Done`)
+    })
+crypto.pbkdf2("password-1", "salt1", 100000, 1024, "sha512", ()=>{
+    console.log(`${Date.now() - start}ms   Done`)
+    })
