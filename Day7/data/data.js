8b9b876fd68177033f28a216c94408c7a27cbdf3
bhvyeahh <bhavyarathore575@gmail.com>
2025-04-15 21:35:44 +0530

    practice

---
+++ Day7/data/data.js
@@ -0,0 +1,29 @@
+const data = [
+    {
+      id: 1,
+      name: "bhavya_rathore",
+      displayName: "Bhavya Rathore"
+    },
+    {
+      id: 2,
+      name: "aisha_khan",
+      displayName: "Aisha Khan"
+    },
+    {
+      id: 3,
+      name: "rehan_malik",
+      displayName: "Rehan Malik"
+    },
+    {
+      id: 4,
+      name: "sana_ahmed",
+      displayName: "Sana Ahmed"
+    },
+    {
+      id: 5,
+      name: "arjun_verma",
+      displayName: "Arjun Verma"
+    }
+  ];
+  
+  export default data
--- Day7/index.js
+++ Day7/index.js
@@ -1,1 +1,48 @@
-console.log("Hello, World!");
+import express from 'express'
+import data from './data/data.js';
+let app = express();
+const PORT = 8888;
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Basic route
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+// GET route to fetch all users
+app.get('/api/v1/users', (req, res) => {
+    res.status(200).send(data);
+});
+
+// Params route to fetch a user by ID
+app.get('/api/v1/users/:id', (req, res) => {
+    const userId = parseInt(req.params.id);
+    const user = data.find(user => user.id === userId);
+
+    if (user) {
+        res.status(200).send(user);
+    } else {
+        res.status(404).send({ message: 'User not found' });
+    }
+});
+
+// PUT route to update user data
+app.put('/api/v1/users/:id', (req, res) => {
+    const userId = parseInt(req.params.id)
+    const userIndex = data.findIndex(user => user.id === userId)
+    if (userIndex !== -1) {
+        const updatedUser = { ...data[userIndex], ...req.body };
+        data[userIndex] = updatedUser;
+        res.status(200).send(updatedUser);
+    } else {
+        res.status(404).send({ message: 'User not found' });
+    }
+});
+
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT:${PORT}`);
+});
