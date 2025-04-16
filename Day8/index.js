3198766263f77bec125d7d6d7aad984b89700e88
bhvyeahh <bhavyarathore575@gmail.com>
2025-04-16 15:05:00 +0530

    day1 middleware

--- Day8/index.js
+++ Day8/index.js
@@ -1,22 +1,10 @@
 import express from 'express';
 
 const app = express();
-// Global middleware
-function hiMiddleware(req, res, next) {
-  console.log('hi middleware');
-  next();
-}
 
-// app.use(hiMiddleware); // commenting this line will not run the middleware
 const PORT = 8080;
-// app.get('/', (req, res) => {
-//   res.send('Hello World!');
-// }); 
 
-
-// Specific middleware for a route
-// we can create multiple middleware and use them in the route
-app.get('/',hiMiddleware, (req, res) => {
+app.get('/', (req, res) => {
     res.send('Hello World!');
   }); 
 
@@ -28,10 +16,3 @@ app.get('/about', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
-
-
-
-
-// 1. global middleware
-// 2. router middleware
-// 3. inbuilt middleware
