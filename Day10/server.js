0557f6b17b6f19e5d1007bdb5cac258925e2e3fd
bhvyeahh <bhavyarathore575@gmail.com>
2025-04-19 16:00:44 +0530

    cookies

---
+++ Day10/server.js
@@ -0,0 +1,36 @@
+import express from "express"
+import cookieParser from "cookie-parser";
+
+const app = express()
+const PORT = 8080;
+
+app.use(cookieParser())
+
+app.get('/', (req, res)=>{
+    // how to set cookie
+    res.cookie("name", "express", {
+        // Age set for cookie
+        maxAge: 1000 * 60 * 60 * 24
+    })
+    res.send("Hello World")
+})
+
+app.get("/product", (req, res)=>{
+    console.log('Cookies: ', req.cookies)
+    if(req.cookies.name && req.cookies.name === "express"){
+    res.status(200).send({id: 1, name: "Item 01", price: "$100"})   
+    }
+    else{
+        res.status(403).send("you are Unauthorized")
+    }
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Server is conncected at http://localhost:${PORT}`)
+})
+
+    // console.log(req.cookies);   undefined
+    // console.log(req.headers.cookie)
+    // console.log('Cookies: ', req.cookies)
+
+    
